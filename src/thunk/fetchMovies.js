89d2f4ff8585563042e1d24d030029e55715f50c
Berdinazar <slybrd01@gmail.com>
2024-04-29 23:28:27 +0300

    used env

--- src/thunk/fetchMovies.js
+++ src/thunk/fetchMovies.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export const fetchMovies = createAsyncThunk("movies/fetch", async () => {
   const response = await axios.get("https://api.kinopoisk.dev/v1.4/movie", {
     headers: {
-      "X-API-KEY": "0GQ42M1-B7P4HA9-NQ4BGVJ-6182G1S",
+      "X-API-KEY": process.env.REACT_APP_API_KEY
     },
   });
   return response.data.docs;
